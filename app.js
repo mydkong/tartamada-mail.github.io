87fdb7a55d74b645a488a3548b736309abb0a760
mydkong <contact.donkeykong@protonmail.com>
2025-12-29 12:05:04 +0100

    Log requests for root and wildcard routes

--- app.js
+++ app.js
@@ -7,7 +7,13 @@ app.use(express.static(path.join(process.cwd(), 'src')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'src', 'index.html'));
+  console.info(req)
 });
+
+app.get('*' (req, res) => {
+  //res.sendFile(path.join(__dirname, 'src', ''));
+  console.info(req)
+})
 
 app.listen(PORT, () => {
   console.log(`Serveur en ligne !`);
