5bf41e99be513baf3153992670d1f32729faf7a3
mydkong <contact.donkeykong@protonmail.com>
2025-12-29 13:44:24 +0100

    Fix routes to serve index.html correctly

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ const PORT = 443
 app.use(express.static(path.join(process.cwd(), 'src')));
 
 app.get('/', (req, res) => {
-  //res.sendFile(path.join(__dirname, 'src', 'index.html'));
-  res.send(req)
+  res.sendFile(path.join(__dirname, 'src', 'index.html'));
+  //res.send(req)
 });
 
-app.get('*' (req, res) => {
-  //res.sendFile(path.join(__dirname, 'src', ''));
-  res.send(req)
+app.get('/*' (req, res) => {
+  res.sendFile(path.join(__dirname, 'src', ''));
+  //res.send(req)
 })
 
 app.listen(PORT, () => {
