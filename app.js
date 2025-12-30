11cbc1d1688e2989bf20323fd7ddf7d75c03f051
mydkong <contact.donkeykong@protonmail.com>
2025-12-30 13:54:53 +0100

    Update app.js

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ app.get('/', (req, res) => {
 });
 
 app.get('/test', (req, res) => {
-  const filePath = path.join(__dirname, 'src', req.url.substr(1))
-  console.log(filePath)
+  /*const filePath = path.join(__dirname, 'src', req.url.substr(1))
+  console.log(filePath)*/
   //if (fs.existsSync(filePath))
     //res.sendFile(filePath)
   /*else
